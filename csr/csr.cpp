8371141fc1a469b2113143f0a331b42a601cff70
Ábner Neves <abnermneves@gmail.com>
2020-11-11 18:41:10 -0300

    solução O(1) espaço e O(n) tempo implementada. novos testes adicionados

--- csr/csr.cpp
+++ csr/csr.cpp
@@ -21,6 +21,37 @@ int ausenteOnOn(int* nums, unsigned int m){
     return i;
 }
 
+// complexidades O(1) de espaço e O(n) de tempo
+int ausenteO1On(int* nums, unsigned int m){
+    unsigned int proximo = 0;
+    int identificado = -1;
+    int temp;
+    unsigned int marcacoes = 0; // contador. vai fazer no total m marcações
+    while (marcacoes < m){
+        // se carro já está marcado
+        if (identificado < 0){
+            // encontra o próximo carro identificado ainda não marcado
+            while (nums[proximo] < 0 && proximo < m)
+                proximo++;
+
+            identificado = nums[proximo];
+            nums[proximo] = -1;
+
+        }
+        // marca o índice correspondente ao carro identificado
+        temp = nums[identificado];
+        nums[identificado] = -2;
+        identificado = temp;
+        marcacoes++;
+    }
+
+    // procura pelo carro não marcado e é ele a resposta
+    unsigned int i;
+    for (i = 0; i < m+1 && nums[i] != -1; i++);
+
+    return i;
+}
+
 int main () {
     unsigned int m;
     cin >> m;
@@ -31,7 +62,7 @@ int main () {
         cin >> nums[i];
     nums[m] = -1;
 
-    resultado = ausenteOnOn(nums, m);
+    resultado = ausenteO1On(nums, m);
 
     cout << "Falta: " << resultado << endl;
     return 0;
