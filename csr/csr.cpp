fed2a1882cdbd972dcefca93f21e91e5df89924f
Ábner Neves <abnermneves@gmail.com>
2020-11-11 12:31:13 -0300

    uns comentários adicionados

--- csr/csr.cpp
+++ csr/csr.cpp
@@ -2,16 +2,20 @@
 
 using namespace std;
 
+// complexidade O(n) tanto de espaço quanto de tempo
 int ausenteOnOn(int* nums, unsigned int m){
     unsigned int i;
     bool identificado[m+1];
 
-    for (i = 0; i <=m; i++)
+    // inicializando o vetor que marca quem foi identificado
+    for (i = 0; i <= m; i++)
         identificado[i] = false;
 
+    // marcando quem foi identificado
     for (i = 0; i < m; i++)
         identificado[nums[i]] = true;
 
+    // encontrando quem não foi identificado
     for (i = 0; i <= m && identificado[i]; i++);
 
     return i;
